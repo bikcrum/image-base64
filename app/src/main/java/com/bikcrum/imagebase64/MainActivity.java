49e9b41e5641990f09173d368bb5f1da31fd2ce4
Bikram Pandit <bikcrum@gmail.com>
2018-01-13 21:26:29 +0545

    coded

--- app/src/main/java/com/bikcrum/imagebase64/MainActivity.java
+++ app/src/main/java/com/bikcrum/imagebase64/MainActivity.java
@@ -1,13 +1,93 @@
 package com.bikcrum.imagebase64;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button pickBtn;
+    private ImageView imageOriginal;
+    private EditText encodedText;
+    private Button decodeBtn;
+    private ImageView imageChanged;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pickBtn = findViewById(R.id.pick_btn);
+        imageOriginal = findViewById(R.id.image_org);
+        encodedText = findViewById(R.id.encoded);
+        decodeBtn = findViewById(R.id.decode_btn);
+        imageChanged = findViewById(R.id.image_cng);
+
+        pickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+            }
+        });
+
+        decodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Glide.with(MainActivity.this).load(encodedText.getText().toString()).into(imageChanged);
+            }
+        });
+    }
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.NO_WRAP);
+
+
+        return encodedImage;
+    }
+
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+                Glide.with(this).load(bitmap).into(imageOriginal);
+
+                encodedText.setText(getStringImage(bitmap));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
